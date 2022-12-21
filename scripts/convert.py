2fa2e2568d9a44d80304e3dbb5e2d32d89276cb2
Shayne Miel <smiel@duosecurity.com>
2022-12-21 11:03:13 -0500

    Generated the final output data

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,10 +1,13 @@
 import argparse
+from dataclasses import asdict
+import json
 from pathlib import Path
+from typing import Any
 from zipfile import PyZipFile
 
 from clean import clean_corpus
-from corpus import load_corpus
-from sentences import parse_sentences
+from corpus import Language, load_corpus
+from sentences import Sentence, parse_sentences
 
 
 def main(zip_path: Path, temp_folder: Path, final_folder: Path) -> None:
@@ -32,9 +35,12 @@ def main(zip_path: Path, temp_folder: Path, final_folder: Path) -> None:
     # create sentences
     sentences = parse_sentences(cleaned_corpus)
 
-    print(sentences[0])
-    print("------------")
-    print(sentences[100])
+    # save as JSON lines files
+    save_to_final(
+        final_folder=final_folder,
+        sentences=sentences,
+        languages=cleaned_corpus.language
+    )
 
 
 def extract(zip_path: Path, temp_folder: Path) -> list[Path]:
@@ -43,6 +49,64 @@ def extract(zip_path: Path, temp_folder: Path) -> list[Path]:
     names = zip.namelist()
     zip.extractall(temp_folder)
     return [temp_folder / name for name in names]
+
+
+def save_to_final(
+    final_folder: Path, sentences: list[Sentence], languages: dict[int, Language]
+) -> None:
+    """
+    Save the sentences as JSON lines files. Create a file of all languages, as
+    well as one file per langauge. Also create a data file that describes the
+    languages.
+    """
+    # parse the date from the final folder
+    date = str(final_folder).split("-")[-1]
+
+    # save all
+    all_path = final_folder / f"dlc-{date}-all.jsonl"
+    write_json_lines(sentences=sentences, path=all_path)
+
+    lang_data = []
+
+    for language in languages.values():
+        lang_path = final_folder / f"dlc-{date}-{language.lang_attribute}.jsonl"
+        lang_sentences = [
+            sentence for sentence in sentences
+            if sentence.language == language.lang_attribute
+        ]
+        if len(lang_sentences) > 0:
+            write_json_lines(sentences=lang_sentences, path=lang_path)
+        else:
+            print(
+                f"Found no sentences for {language.language} ({language.lang_attribute})"
+            )
+        lang_data.append(
+            dict(
+                language=language.language,
+                abbreviation=language.lang_attribute,
+                n_sentences=len(lang_sentences)
+            )
+        )
+
+    # write out a data file
+    data_path = final_folder / f"data-dlc-{date}.json"
+    data = dict(
+        languages=lang_data
+    )
+    with open(data_path, "w") as fout:
+        json.dump(data, fout)
+
+
+def write_json_lines(sentences: list[Sentence], path: Path) -> None:
+    """Write sentences as JSON lines"""
+    print(f"Writing {len(sentences)} sentences to {path}")
+    json_lines = []
+    for sentence in sentences:
+        line = json.dumps(asdict(sentence), ensure_ascii=False)
+        json_lines.append(line)
+
+    with open(path, "w") as fout:
+        fout.writelines(json_lines)
 
 
 def create_arg_parser() -> argparse.ArgumentParser:
